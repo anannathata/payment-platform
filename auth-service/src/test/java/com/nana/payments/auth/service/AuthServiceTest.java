dd0a621e67c5821b45798528b81187fd1eac3e1f
Nana <marimotep@gmail.com>
2026-05-19 02:01:01 -0300

    test(auth-service): add valid login unit test

---
+++ auth-service/src/test/java/com/nana/payments/auth/service/AuthServiceTest.java
@@ -0,0 +1,37 @@
+package com.nana.payments.auth.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.nana.payments.auth.dto.LoginRequest;
+
+@ExtendWith(MockitoExtension.class)
+
+public class AuthServiceTest {
+
+    @Mock
+    JwtService jwtService;
+
+    @InjectMocks
+    AuthService authService;
+
+    @Test
+    void shouldReturnLoginResponseWhenCredentialsAreValid() {
+
+        when(jwtService.generateToken("admin")).thenReturn("fake-token");
+        when(jwtService.getExpiration()).thenReturn(3600000L);
+
+        var response = authService.login(new LoginRequest("admin", "admin123"));
+
+        assertEquals("fake-token", response.accessToken());
+        assertEquals("Bearer", response.tokenType());
+        assertEquals(3600000L, response.expiresIn());
+
+    }
+}
